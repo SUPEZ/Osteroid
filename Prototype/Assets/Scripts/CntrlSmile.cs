6e9810ac27a1ca5c47721de7b16aacd53e20fc28
NIKICH <cyberflower999@gmail.com>
2020-11-20 16:03:13 +0300

    THANOS 0.1v

---
+++ Prototype/Assets/Scripts/CntrlSmile.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CntrlSmile : MonoBehaviour
+{
+    public GameController gameController;
+    public Sprite goodSmile;
+    public Sprite badSmile;
+    private float speed, tilt;
+    private Vector3 target;
+    private bool anger = false;
+    private int localScore = 0;
+
+    private void Start()
+    {
+        speed = gameController.speed;
+        tilt = gameController.tilt;
+        ChangeTarget();
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        transform.Rotate(Vector3.back * tilt);
+        if (localScore % 5 == 0 && localScore != 0)
+        {
+            anger = true;
+            tilt = Random.Range(-10, 10);
+            GetComponent<SpriteRenderer>().sprite = badSmile;  
+        }else if (localScore % 5 != 0 && anger == true)
+        {
+            anger = false;
+            GetComponent<SpriteRenderer>().sprite = goodSmile;
+        }
+        if (gameController.cntScore == 20)
+        {
+            speed = gameController.speed;
+        }
+    }
+    
+    private void OnCollisionEnter(Collision nearObj)
+    {
+        
+        if (nearObj.gameObject.tag == "Wall")
+        {
+            Debug.Log(nearObj.gameObject.name);
+            if (anger == true)
+            {
+                localScore += 1;
+                gameController.cntScore += 1;
+                ChangeTarget();
+                
+            }else if (anger == false)
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+        if (nearObj.gameObject.tag == "Smile")
+        {
+            if (anger == true && nearObj.gameObject.GetComponent < CntrlSmile > ().anger == false)
+            {
+                localScore += 1;
+                gameController.cntScore += 1;
+                Destroy(nearObj.gameObject);
+
+            }
+            else if (anger == true && nearObj.gameObject.GetComponent<CntrlSmile>().anger == true)
+            {
+                ChangeTarget();
+            }
+            else if (anger == false && nearObj.gameObject.GetComponent<CntrlSmile>().anger == false)
+            {
+                ChangeTarget();
+            }
+        }
+        
+    }
+
+    void OnMouseUpAsButton()
+    {
+        if (anger)
+        {
+            Destroy(gameObject);
+            return;
+        }else if (!anger)
+        {
+            gameController.cntScore += 1;
+            localScore += 1;
+        }
+        if (gameController.semaphoreForSpawn == false)
+        {
+            gameController.semaphoreForSpawn = true;
+        }
+        if (gameController.semaphoreForSpeed == false)
+        {
+            gameController.semaphoreForSpeed = true;
+        }
+        ChangeTarget();
+    }
+
+    private void ChangeTarget()// смена конечной точки
+    {
+        if (gameObject.transform.position.x >= 0 && gameObject.transform.position.y >= 0)
+        {
+            target = new Vector3(Random.Range(-10, 0) * 100, Random.Range(-10, 0) * 100, 0);
+        }
+        else if (gameObject.transform.position.x < 0 && gameObject.transform.position.y >= 0)
+        {
+            target = new Vector3(Random.Range(0, 10) * 100, Random.Range(-10, 0) * 100, 0);
+        }
+        else if (gameObject.transform.position.x < 0 && gameObject.transform.position.y < 0)
+        {
+            target = new Vector3(Random.Range(0, 10) * 100, Random.Range(0, 10) * 100, 0);
+        }
+        else if (gameObject.transform.position.x >= 0 && gameObject.transform.position.y < 0)
+        {
+            target = new Vector3(Random.Range(-10, 0) * 100, Random.Range(0, 10) * 100, 0);
+        }
+    }
+}
---
+++ Prototype/Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public float speed, tilt;
+    public GameObject goodSmile;
+    public Text score;
+    public int cntScore = 0;
+
+    public bool semaphoreForSpawn = true;
+    public bool semaphoreForSpeed = true;
+    
+    private ArrayList smiles = new ArrayList();
+    private Vector3 spawnPosition;
+    
+    void Start () {
+        
+        spawnPosition = new Vector3(Random.Range(-1.5f, 1.5f), Random.Range(-3.5f, 3.5f), 0);
+        AddSmile();
+    }
+	
+	void Update () {
+        score.text = cntScore.ToString();
+        if (cntScore != 0 && cntScore % 10 == 0 && semaphoreForSpawn == true) {
+            AddSmile();
+            semaphoreForSpawn = false;
+        }
+        if (cntScore == 20 && semaphoreForSpeed == true)
+        {
+            speed += 0.5f;
+            semaphoreForSpeed = false;
+        }
+    }
+
+    void AddSmile ()
+    {
+        GameObject smile = Instantiate(goodSmile, spawnPosition, Quaternion.identity);
+        smile.GetComponent<CntrlSmile>().gameController = this;
+        smiles.Add(smile);
+    }
+}
