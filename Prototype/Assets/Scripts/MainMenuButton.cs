de509e135f0fc475a410eb663656ddb2a117ddf3
NIKICH <cyberflower999@gmail.com>
2020-11-24 16:52:16 +0300

    THANOS 0.2v
    Main Menu Scene and buttons

---
+++ Prototype/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour {
+
+    public Sprite SpriteOnMouseUpButton, SpriteOnMouseDownButton;
+
+    private void OnMouseDown()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        switch (gameObject.name)
+        {
+            case "PlayButton":
+                SceneManager.LoadScene("Play");
+                break;
+        }
+    }
+}
